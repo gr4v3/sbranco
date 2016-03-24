b2ffed5f9bc1e515497ce35417a8451b93362dc0
fabio menezes <fabio menezes@office-dev.simples.pt>
2016-03-24 21:22:24 +0000

    #version #asdasdas

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,9 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//$this->load->view('welcome_message');
+            $this->load->library('page');
+            $items = $this->page->items();
+            Debug($items);
 	}
 }
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,89 +1,98 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
-</head>
-<body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
-</body>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>A fotógrafa</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/index.css" rel="stylesheet" type="text/css"/>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    </head>
+    <body class="body">
+        <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=225678960794287";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+        <div class="main">
+            <div class="author col-md-12">
+                <h1>sandra branco</h1>
+                <h2>photography</h2>
+                <div class="fb-like" data-href="https://sandrabranco.com" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div>
+            </div>
+            <div class="menu col-md-3">
+                <ul class="nav">
+                    <li><a title="people">people</a></li>
+                    <li><a title="portraits">portraits</a></li>
+                    
+                    <li><a title="cityscapes">cityscapes</a></li>
+                    <li><a title="landscapes">landscapes</a></li>
+                    <li><a title="in loko tells a story"><i>in loco</i><span>tells a story</span></a></li>
+                    <li><a title="black & white">black & white</a></li>
+                    <li><a title="report">report</a></li>
+                    <li><a title="in motion">in motion</a></li>
+                    <li><a title="contact me">contact me</a></li>
+                    
+                    <li><a title="about me">about me</a></li>
+                </ul>
+            </div>
+            <div class="content col-md-9">
+                <div class="slide">
+                    <table class="gallery" cellpadding="0" cellspacing="0" border="0">
+                        <tr>
+                            <td class="item view">
+                                <img src="gallery/12895294_10153250903237924_1629883350_n.jpg" alt=""/>
+                            </td>
+                            <td class="item">
+                                <img src="gallery/12895379_10153250898017924_602496037_n.jpg" alt=""/>
+                            </td>
+                            <td class="item">
+                                <img src="gallery/12874419_10153251069127924_230922786_o.jpg" alt=""/>
+                            </td>
+                            <td class="item">
+                                <img src="gallery/12422312_10153251081582924_1995153778_o (1).jpg" alt=""/>
+                            </td>
+                            <td class="item">
+                                <img src="gallery/12443635_10153251081502924_1170462370_o.jpg" alt=""/>
+                            </td>
+                            <td class="item">
+                                <img src="gallery/12596754_10153251081772924_1052114118_o.jpg" alt=""/>
+                            </td>
+                            <td class="item">
+                                <img src="gallery/12874558_10153251074872924_1974357794_o.jpg" alt=""/>
+                            </td>
+                            <td class="item">
+                                <img src="gallery/12874219_10153251346387924_85925787_o.jpg" alt=""/>
+                            </td>
+                            <td class="item">
+                                <img src="gallery/12887455_10153251074732924_1295252013_o.jpg" alt=""/>
+                            </td>
+                        </tr>   
+                    </table>
+                </div>
+            </div>
+        </div>
+        <script src="js/jquery-1.12.2.min.js" type="text/javascript"></script>
+        <script src="js/jquery.touchSwipe.min.js" type="text/javascript"></script>
+        <script src="js/jquery.scrollTo.min.js" type="text/javascript"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <script src="js/index.js" type="text/javascript"></script>
+        <audio controls autoplay>
+            <source src="audio/thexx.mp3" type="audio/mpeg">
+            Your browser does not support the audio element.
+         </audio>
+    </body>
 </html>
