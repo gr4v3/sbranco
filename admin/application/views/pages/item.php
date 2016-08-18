28958ada6c7955c1429b46685dddc5ceecd679b5
Fábio Menezes <gr4v3m4n@gmail.com>
2016-08-18 23:30:37 +0100

    Update item.php

--- admin/application/views/pages/item.php
+++ admin/application/views/pages/item.php
@@ -7,7 +7,7 @@
  */
 ?>
 <div class="col-md-2 item">
-    <div onclick="window.location = '/admin/pages/index/<?php echo $link; ?>';" ondragover="Gallery.allowdrop(event)" ondrop="Gallery.drop(event);" draggable="true" ondragstart="Gallery.dragstart(event)" ondragenter="Gallery.dragenter(event);" style="background-image:url(<?php echo '/img-auto-115/assets/pages/' . $link . '/' . current($items); ?>);">
+    <div title="<?php echo $name; ?>" onclick="window.location = '/admin/pages/index/<?php echo $link; ?>';" ondragover="Gallery.allowdrop(event)" ondrop="Gallery.drop(event);" draggable="true" ondragstart="Gallery.dragstart(event)" ondragenter="Gallery.dragenter(event);" style="background-image:url(<?php echo '/img-auto-115/assets/pages/' . $link . '/' . current($items); ?>);">
         <input type="hidden" name="page[]" value="<?php echo $link; ?>" />
     </div>   
 </div>
