eae543067e6f16021f3d0ffa05e223a3002af3d2
root <root@production.admedia.pt>
2016-08-19 04:00:22 +0100

    asdasdas

---
+++ admin/application/views/media/form/gallery/item.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<div class="col-md-2 item">
+    <div draggable="true" ondragstart="Gallery.dragstart(event)" ondragenter="Gallery.dragenter(event);" style="background-image:url(<?php echo '/img-auto-115/assets/pages/' . $page . '/' . $src; ?>);">
+        <input type="hidden" name="page[items][]" value="<?php echo $src; ?>" />
+    </div>   
+</div>
---
+++ admin/application/views/media/item.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<div class="col-md-2 item">
+    <div title="<?php echo $title; ?>" onclick="window.location = '/admin/pages/index/<?php echo $link; ?>';" ondragover="Gallery.allowdrop(event)" ondrop="Gallery.drop(event);" draggable="true" ondragstart="Gallery.dragstart(event)" ondragenter="Gallery.dragenter(event);" style="background-image:url(<?php echo '/img-auto-115/assets/pages/' . $link . '/' . current($items); ?>);">
+        <input type="hidden" name="page[]" value="<?php echo $link; ?>" />
+    </div>   
+</div>
