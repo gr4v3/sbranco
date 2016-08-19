93be26fad9bd1b0cd9931d16d5e45a4cf1e40a2c
Fábio Menezes <gr4v3m4n@gmail.com>
2016-08-19 05:07:54 +0100

    Create gallery.php

---
+++ admin/application/views/media/gallery.php
@@ -0,0 +1,25 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<form id="pages-form" target="pages-form-target" action="/admin/track/update" method="post">
+<div class="container-fluid">
+    <div class="col-md-12 gallery">
+        <div class="col-md-2 item add">
+            <a href="/admin/track/add">
+                <div class="fa fa-plus"></div>
+            </a>
+        </div>
+        <div ondrop="Gallery.clear()" ondragover="Gallery.allowdrop(event)" class="col-md-2 item fa fa-trash">
+            <div>arraste para aqui se deseja apagar a foto</div>
+        </div>
+        <?php 
+            echo $items;
+        ?>
+    </div>
+</div>
+</form>
+<iframe name="pages-form-target" style="display:none;"></iframe>
