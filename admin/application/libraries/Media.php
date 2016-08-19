86ca4f2a31f10f340829a6e6fd37452899c6df15
Fábio Menezes <gr4v3m4n@gmail.com>
2016-08-19 04:48:51 +0100

    Update Media.php

--- admin/application/libraries/Media.php
+++ admin/application/libraries/Media.php
@@ -12,51 +12,13 @@ class Media {
         $this->CI =& get_instance();
     }
     public function items() {
-        $pages_path = CLIENTPATH . 'assets/audio';
-        if (!is_dir($pages_path)) mkdir($pages_path, 0777, TRUE);
-        $pages = scandir($pages_path);
-        foreach($pages as &$page) {
-            if (in_array($page, array('.', '..'))) $page = NULL;
-            else $page = $this->get($page);
+        $audio_path = CLIENTPATH . 'assets/audio';
+        if (!is_dir($audio_path)) mkdir($audio_path, 0777, TRUE);
+        $audios = scandir($audio_path);
+        foreach($audios as &$audio) {
+            if (in_array($audio, array('.', '..'))) $audio = NULL;
         }
         $pages_filtered = array_filter($pages);
-        $pages_result = array();
-        foreach($pages_filtered as $item) {
-            $pages_result[(int) $item->index] = $item;
-        }
-        ksort($pages_result);
-        return $pages_result;
-    }
-    public function set($name = NULL, $options = array()) {
-        $page_path = CLIENTPATH . 'assets/audio/' . $name;
-        if (!is_dir($page_path)) mkdir($page_path, 0777, TRUE);
-        file_put_contents($page_path. '/.options', json_encode($options));
-        file_put_contents($page_path. '/.htaccess', 'RewriteRule ^img-([0-9]{1,4}|\bauto\b)?-([0-9]{1,4}|\bauto\b)?/(([A-Za-z0-9/_-]+).(jpg|gif|png))?$ /home/sbranco/resize.php?width=$1&height=$2&imgfile=$3');
-    }
-    public function get($name = NULL) {
-        $page_path = CLIENTPATH . 'assets/audio/' . $name;
-        if (!is_dir($page_path)) return NULL;
-        return json_decode(file_get_contents($page_path. '/.options'));
-    }
-    public function del($name = NULL) {
-        $page_path = CLIENTPATH . 'assets/audio/' . $name;
-        if (!is_dir($page_path)) return NULL;
-        $this->rrmdir($page_path);
-    }
-    public function rrmdir($src) {
-        $dir = opendir($src);
-        while(false !== ( $file = readdir($dir)) ) {
-            if (( $file != '.' ) && ( $file != '..' )) {
-                $full = $src . '/' . $file;
-                if ( is_dir($full) ) {
-                    rrmdir($full);
-                }
-                else {
-                    unlink($full);
-                }
-            }
-        }
-        closedir($dir);
-        rmdir($src);
+        return $pages_filtered;
     }
 }
