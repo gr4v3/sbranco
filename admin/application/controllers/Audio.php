0fc8acf2bf24e56b34a627c0f63f501bfe6781d0
Fábio Menezes <gr4v3m4n@gmail.com>
2016-08-19 03:16:32 +0100

    Update Audio.php

--- admin/application/controllers/Audio.php
+++ admin/application/controllers/Audio.php
@@ -20,11 +20,47 @@ class Audio extends MY_Controller {
 	 */
 	public function index($index = NULL)
 	{
-            
-            
-            $this->load->view('admin', array('page' => ''));
+            $this->load->library('media');
+            if (empty($index)) {
+                $pages = $this->media->items();
+                $this->load->view('admin', array('page' => $this->load->view('media/form/item', array(
+                    'index' => count($pages),
+                    'title' => '', 
+                    'link' => '', 
+                    'audio' => '', 
+                    'background' => '#000000', 
+                    'fontcolor' => '#ffffff',
+                    'type' => 'slide', 
+                    'gallery' => ''
+                    ), TRUE)));
+            } else {
+                $page = $this->media->get($index);
+                $gallery_items = array();
+                foreach($page->items as $item) {
+                    $gallery_items[] = $this->load->view('media/form/gallery/item', array('page' => $index, 'src' => $item), TRUE);
+                }
+                $page->gallery = implode('', $gallery_items);
+                
+                $this->load->view('admin', array('page' => $this->load->view('media/form/item', $page, TRUE)));
+            }
 	}
         public function update() {
-            
+            $this->load->library('media');
+            $pages = $this->media->items();
+            $currentPages = array_map(function($item) {return $item->link;}, $pages);
+            $postpages = $this->input->post('media');
+            if (empty($postpages)) $postpages = array();
+            $toDelete = array_diff_key(array_flip($currentPages), array_flip($postpages));
+            foreach(array_flip($toDelete) as $link) {
+                $this->media->del($link);
+            }
+            foreach($pages as $item) {
+                foreach($postpages as $pageindex => $pagelink) {
+                    if ($pagelink === $item->link) {
+                        $item->index = $pageindex;
+                        $this->page->set($item->link, $item);
+                    }
+                }
+            }
         }
 }
