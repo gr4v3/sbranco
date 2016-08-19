aef7839c08e851432428a7e77b3b5575e3484b52
Fábio Menezes <gr4v3m4n@gmail.com>
2016-08-19 06:07:07 +0100

    Update item.php

--- admin/application/views/media/item.php
+++ admin/application/views/media/item.php
@@ -8,6 +8,7 @@
 ?>
 <div class="col-md-2 item">
     <div draggable="true" ondragstart="Gallery.dragstart(event)" ondragenter="Gallery.dragenter(event);">
+        <input type="hidden" name="media[]" value="<?php echo $file; ?>" /> 
         <?php echo $file; ?>
     </div>
 </div>
